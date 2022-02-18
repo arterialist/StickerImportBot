a13f1d640bb2d0586c330df76e02af373cbd9047
Arterialist <qazedcwsxrfv228@gmail.com>
2022-02-18 11:46:54 +0300

    change page fetching to selenium, add proper sticker fetching

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import shutil
 
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message, InputFile
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from image_utils import download_image, add_outline, convert_to_webp
 
@@ -19,7 +21,8 @@ async def dice_handler(event: Message):
 
 async def link_handler(event: Message):
     msg_text = event.text
-    link_regexp = r"(https:\/\/vk\.com\/sticker\/\d*-(\d*)-(64|128|256|512))"
+    # sticker_image_link_regexp = r"(https:\/\/vk\.com\/sticker\/\d*-(\d*)-(64|128|256|512))"
+    link_regexp = r"(https:\/\/m\.vk\.com\/stickers/\w*)"
     matches = re.search(link_regexp, msg_text)
 
     if not matches:
@@ -27,25 +30,52 @@ async def link_handler(event: Message):
         await bot.send_message(event.chat.id, "No valid links found in your message")
         return
 
-    sticker_link = matches.groups()[0]
-    starting_id = int(matches.groups()[1])
+    page_link = matches.groups()[0]
     temp_directory_name = f"pack_{event.from_user.id}_{event.message_id}"
     temp_directory_path = f"./data/{temp_directory_name}"
+    print(page_link)
 
     os.mkdir(temp_directory_path)
 
-    stickerset_name: str = None
-    for sticker_id in range(starting_id, starting_id + 42):
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': 'ru,ru_RU'})
+    driver = webdriver.Chrome("/home/arterialist/Downloads/chromedriver/chromedriver", chrome_options=options)
+    driver.get(page_link)
 
-        png_image_path = await download_image(sticker_link.replace(str(starting_id), str(sticker_id)), str(sticker_id), temp_directory_path)
+    # async with ClientSession() as session:
+    #     async with session.get(page_link) as response:
+    #         page_content = await response.text()
+    #         page_content = BeautifulSoup(page_content, features="html.parser")
+
+    stickerpack_name = driver.find_element(By.CLASS_NAME, "stickers_name").text
+    stickerpack_sticker_links = driver.find_elements(By.CLASS_NAME, "sticker_img")
+    stickerpack_sticker_links = list(
+        sorted(
+            map(
+                lambda img: img.get_attribute("src").replace("64", "512").replace("128", "512").replace("256", "512"), stickerpack_sticker_links
+            )
+        )
+    )
+    driver.close()
+
+    stickerset_name: str = None
+    starting_id = None
+
+    for sticker_link in stickerpack_sticker_links:
+        sticker_id = sticker_link.split("/")[-1:][0].split("-")[1]
+
+        if not starting_id:
+            starting_id = sticker_id
+
+        png_image_path = await download_image(sticker_link, sticker_id, temp_directory_path)
         await add_outline(png_image_path)
-        webp_image_path = await convert_to_webp(temp_directory_path, str(sticker_id), "png")
+        webp_image_path = await convert_to_webp(temp_directory_path, sticker_id, "png")
         png_sticker = InputFile(png_image_path)
         webp_sticker = InputFile(webp_image_path)
 
         if not stickerset_name:
             stickerset_name = f"set_{starting_id}_{event.from_user.id}_by_StickerImporterBot"
-            await bot.create_new_sticker_set(event.from_user.id, stickerset_name, "Sticker pack by @StickerImporterBot", "😁", png_sticker)
+            await bot.create_new_sticker_set(event.from_user.id, stickerset_name, f"{stickerpack_name} VK by @StickerImporterBot", "😁", png_sticker)
         else:
             await bot.add_sticker_to_set(event.from_user.id, stickerset_name, "😁", png_sticker)
 
